0e763d7df74c71ef323269227b975239a02bd528
abellepatrice <abellepatrice@gmail.com>
2025-06-12 10:05:45 +0300

    Wrapped PWA and generated APK file

---
+++ app/src/main/java/dev/fly/shopping_manager_app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.fly.shopping_manager_app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
